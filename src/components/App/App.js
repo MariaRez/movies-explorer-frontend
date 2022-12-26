b3783fb5f0b03b343cfcb2a5f2f5164b2512c256
MariaRez <maria.v.reznik@yandex.ru>
2022-12-26 23:40:58 +0400

    feat: make footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
+import Footer from '../Footer/Footer';
 import './App.css';
 
 function App() {
-
+  return (
+    <div className="page">
+      <Footer/>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__info'>
+        <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+        <nav>
+            <ul className='footer__links'>
+                <li><a className='footer__link' href="https://practicum.yandex.ru" target="_blank" rel='noreferrer'>Яндекс.Практикум</a></li>
+                <li><a className='footer__link' href="https://github.com/MariaRez" target="_blank" rel='noreferrer'>Github</a></li>
+            </ul>
+        </nav>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
