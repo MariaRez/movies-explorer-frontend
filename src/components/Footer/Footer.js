613fabdd8f35657b53e9bd21d08b699d10e6f1d7
MariaRez <maria.v.reznik@yandex.ru>
2022-12-27 00:51:40 +0400

    fix: make fix - forget import react in some components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './Footer.css';
 
 function Footer() {
@@ -8,8 +9,8 @@ function Footer() {
         <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
         <nav>
             <ul className='footer__links'>
-                <li><a className='footer__link' href="https://practicum.yandex.ru" target="_blank" rel='noreferrer'>Яндекс.Практикум</a></li>
-                <li><a className='footer__link' href="https://github.com/MariaRez" target="_blank" rel='noreferrer'>Github</a></li>
+                <li><a className='footer__link' href="https://practicum.yandex.ru" target="_blank" rel="noopener noreferrer">Яндекс.Практикум</a></li>
+                <li><a className='footer__link' href="https://github.com/MariaRez" target="_blank" rel="noopener noreferrer">Github</a></li>
             </ul>
         </nav>
       </div>
