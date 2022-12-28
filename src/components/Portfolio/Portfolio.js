868bd63893975d5d5d1c28550265975f39840097
MariaRez <maria.v.reznik@yandex.ru>
2022-12-28 15:44:57 +0400

    fix: style for portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,15 +6,15 @@ function Portfolio() {
         <section className="portfolio">
             <p className="portfolio__text">Портфолио</p>
             <ul className='portfolio__links'>
-                <li className="portfolio__links-container">
+                <li className="portfolio__link-container">
                     <p className='portfolio__link-text'>Статичный сайт</p>
                     <button className="portfolio__link-button">↗</button>
                 </li>
-                <li className="portfolio__links-container">
+                <li className="portfolio__link-container">
                     <p className='portfolio__link-text'>Адаптивный сайт</p>
                     <button className="portfolio__link-button">↗</button>
                 </li>
-                <li className="portfolio__links-container">
+                <li className="portfolio__link-container">
                     <p className='portfolio__link-text'>Одностраничное приложение</p>
                     <button className="portfolio__link-button">↗</button>
                 </li>
