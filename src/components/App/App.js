e730d132997658f30ea56658a7b6a2a295dcbc03
MariaRez <maria.v.reznik@yandex.ru>
2022-12-30 15:24:25 +0400

    feat: make some routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Main from "../Main/Main";
 import Footer from '../Footer/Footer';
 import './App.css';
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="page">
-      <Main/>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/movies">
+        </Route>
+        <Route exact path="/saved-movies">
+        </Route>
+        <Route exact path="/profile">
+        </Route>
+        <Route exact path="/signin">
+        </Route>
+        <Route exact path="/signup">
+        </Route>
+        <Route path="/*">
+          <PageNotFound />
+        </Route>
+      </Switch>
       <Footer/>
     </div>
   )
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './PageNotFound.css';
+
+function PageNotFound() {
+    return (
+        <section className="page-not-found">
+        </section>
+    )
+  }
+  
+  export default PageNotFound;
