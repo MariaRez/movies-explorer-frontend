b927aaae448b3fa98b0e3835e0717e77457e0de3
MariaRez <maria.v.reznik@yandex.ru>
2022-12-30 19:57:54 +0400

    feat: add 404 page styles

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -4,6 +4,12 @@ import './PageNotFound.css';
 function PageNotFound() {
     return (
         <section className="page-not-found">
+            <h1 className="page-not-found__title">404</h1>
+            <p className="page-not-found__subtitle">Страница не найдена</p>
+            <button
+                    aria-label="Back"
+                    type="button"
+                    className="page-not-found__button">Назад</button>
         </section>
     )
   }
