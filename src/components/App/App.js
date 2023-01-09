2a651bc434c55aaa5508bf445e9b43d3b563fff6
MariaRez <maria.v.reznik@yandex.ru>
2023-01-09 14:54:50 +0400

    feat: make component profile, footer just for pages project, films and saved films

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import Main from "../Main/Main";
 import Footer from '../Footer/Footer';
 import './App.css';
 import PageNotFound from "../PageNotFound/PageNotFound";
+import Profile from "../Profile/Profile";
 
 function App() {
+
+  const location = useLocation(); // подвал приложения должен быть на странице о проекте, фильмы и сохраненные фильмы
+
   return (
     <div className="page">
       <Switch>
@@ -17,6 +21,7 @@ function App() {
         <Route exact path="/saved-movies">
         </Route>
         <Route exact path="/profile">
+          <Profile />
         </Route>
         <Route exact path="/signin">
         </Route>
@@ -26,7 +31,7 @@ function App() {
           <PageNotFound />
         </Route>
       </Switch>
-      <Footer/>
+      {location.pathname === "/" || location.pathname === "/movies" || location.pathname === "/saved-movies" ? <Footer /> : ''}
     </div>
   )
 }
