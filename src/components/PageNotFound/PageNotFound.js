a0332c17c34fdb3bb172a203e449b897442ad0f9
MariaRez <maria.v.reznik@yandex.ru>
2023-01-09 15:04:23 +0400

    fix: change link in PageNotFound to aboutProject page (will need to make it to back)

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './PageNotFound.css';
 
 function PageNotFound() {
@@ -6,10 +7,7 @@ function PageNotFound() {
         <section className="page-not-found">
             <h1 className="page-not-found__title">404</h1>
             <p className="page-not-found__subtitle">Страница не найдена</p>
-            <button
-                    aria-label="Back"
-                    type="button"
-                    className="page-not-found__button">Назад</button>
+            <Link to="/" className="page-not-found__button">Назад</Link>
         </section>
     )
   }
