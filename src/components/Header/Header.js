c4e2c328231854479c6b398699142de85991faa1
MariaRez <maria.v.reznik@yandex.ru>
2023-01-12 18:09:05 +0400

    feat: make header for desktop version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
+import React from "react";
 import "./Header.css";
 import logo from "../../images/logo.svg";
+import profile from "../../images/profile.svg";
 import { Link, Route, useLocation } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
 
     const location = useLocation();
-    const headerClassName = (`header ${ location.pathname === "/" ? 'header_color_emerald' : 'header_color_dark'}`);
+    const headerClassName = (`header ${location.pathname === "/" ? 'header_color_emerald' : 'header_color_dark'}`);
 
     return (
       <header className={headerClassName}>
@@ -13,21 +16,19 @@ function Header() {
             <Link to="/">
                 <img className="logo" src={logo} alt="Логотоп" />
             </Link>
-            <Route exact path="/">
-                <div className="header__links header__links_place_main">
-                    <Link to="/signup" className="header__link header__link_to_signup">Регистрация</Link>
-                    <Link to="/signin" className="header__link header__link_to_signin">Войти</Link>
-                </div>
-            </Route>
-            <Route exact path="/movies">
-                <div className="header__links"></div>
-            </Route>
-            <Route exact path="/saved-movies">
-                <div className="header__links"></div>
-            </Route>
-            <Route exact path="/profile">
-                <div className="header__links"></div>
-            </Route>
+            <div className="header__links">
+            {location.pathname === "/" ? // для главной страницы
+            <>
+              <Link to="/signup" className="header__link header__link_color_white">Регистрация</Link>
+              <Link to="/signin" className="header__link header__link_color_green">Войти</Link>
+            </> : // для иных на которых есть header
+            <>
+              <Link to="/movies" className="header__link header__link_color_white">Фильмы</Link>
+              <Link to="/saved-movies" className="header__link header__link_right header__link_color_white">Сохранённые фильмы</Link>
+              <Link to="/profile" className="header__link"><img className="header__link-image" src={profile} alt="Профиль" /></Link>
+            </>
+            }
+             </div>
         </div>
       </header>
   );
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import profile from "../../images/profile.svg";
+import "./Navigation.css";
+
+function Navigation() {
+    return (
+        <nav className="navigation">
+        </nav>
+  );
+}
+
+export default Navigation;
