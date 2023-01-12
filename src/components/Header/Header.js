23e132d812df221278394e719e38ef701ada6046
MariaRez <maria.v.reznik@yandex.ru>
2023-01-12 20:02:45 +0400

    feat: make burger-menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
+import {useState} from "react";
 import "./Header.css";
 import logo from "../../images/logo.svg";
 import profile from "../../images/profile.svg";
-import { Link, Route, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 
 function Header() {
 
-    const location = useLocation();
-    const headerClassName = (`header ${location.pathname === "/" ? 'header_color_emerald' : 'header_color_dark'}`);
+  const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
+
+  const location = useLocation();
+  const headerClassName = (`header ${location.pathname === "/" ? 'header_color_emerald' : 'header_color_dark'}`);
+
+  function handleBurgerMenuButton () {
+    setIsOpenBurgerMenu(!isOpenBurgerMenu);
+  }
 
     return (
       <header className={headerClassName}>
@@ -23,13 +30,15 @@ function Header() {
               <Link to="/signin" className="header__link header__link_color_green">Войти</Link>
             </> : // для иных на которых есть header
             <>
-              <Link to="/movies" className="header__link header__link_color_white">Фильмы</Link>
-              <Link to="/saved-movies" className="header__link header__link_right header__link_color_white">Сохранённые фильмы</Link>
-              <Link to="/profile" className="header__link"><img className="header__link-image" src={profile} alt="Профиль" /></Link>
+              <Link to="/movies" className="header__link header__link_color_white header__link_hidden" >Фильмы</Link>
+              <Link to="/saved-movies" className="header__link header__link_right header__link_color_white header__link_hidden">Сохранённые фильмы</Link>
+              <Link to="/profile" className="header__link"><img className="header__link-image header__link_hidden" src={profile} alt="Профиль" /></Link>
+              <button aria-label="Open navigation" className="header__burger-menu" type="button" onClick={handleBurgerMenuButton}/>
             </>
             }
-             </div>
+            </div>
         </div>
+        {isOpenBurgerMenu ? <Navigation handleBurgerMenuButton={handleBurgerMenuButton}/> : ''}
       </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,10 +3,17 @@ import { Link } from "react-router-dom";
 import profile from "../../images/profile.svg";
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation( { handleBurgerMenuButton }) {
     return (
-        <nav className="navigation">
-        </nav>
+        <section className="navigation">
+          <button aria-label="Close navigation" className="navigation__close-button" type="button" onClick={handleBurgerMenuButton}/>
+          <nav className="navigation__links">
+            <Link to="/" className="navigation__link" onClick={handleBurgerMenuButton}>Главная</Link>
+            <Link to="/movies" className="navigation__link navigation__link_checked" onClick={handleBurgerMenuButton}>Фильмы</Link>
+            <Link to="/saved-movies" className="navigation__link" onClick={handleBurgerMenuButton}>Сохранённые фильмы</Link>
+            <Link to="/profile" className="navigation__link" onClick={handleBurgerMenuButton}><img className="navigation__link-image" src={profile} alt="Профиль"/></Link>
+          </nav>
+        </section>
   );
 }
 
