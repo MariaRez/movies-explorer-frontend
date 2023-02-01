d71697ea2298de8098d66f9b753c9cdad01f019f
MariaRez <maria.v.reznik@yandex.ru>
2023-02-01 19:13:41 +0400

    feat: added functionality for the appearance or disappearance of the button more

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -10,10 +10,48 @@ function MoviesCardList({
   filterStatus,
 }) {
 
+  const [width, setWidth] = useState(0);   // ширина
+  const [amount, setAmount] = useState(0); // счетчик
+  const part = movies.slice(0, amount); // разделение, необходимое для отрисовки
+
+  // чтобы колбэк-функция слушателя не срабатывала слишком часто, установливаем setTimeout на вызов этой функции внутри слушателя "resize"
+  function screenWidthChange() {
+    setTimeout(() => {  
+      setWidth(window.innerWidth);
+    }, 500);
+  }
+
+  // при изменении ширины экрана
+  useEffect(() => {
+    window.addEventListener("resize", screenWidthChange);
+    setWidth(window.innerWidth);
+    if (width >= 1280) {
+      setAmount(12);
+    } else if (width >= 768) {
+      setAmount(8);
+    } else {
+      setAmount(5);
+    }
+    return () => {
+      window.removeEventListener("resize", screenWidthChange);
+    };
+  }, [width]);
+
+  // устанавливаем значение при нажитии на кнопку еще
+  const handleMoreCards = () => {
+    if (width >= 1280) {
+     setAmount(amount + 3);
+   } else if (width >= 768) {
+     setAmount(amount + 2);
+   } else {
+     setAmount(amount + 2);
+   }
+ };
+
   return (
     <section className="movies-card-list">
       <ul className="movies-card-list__movies">
-        {movies.map((movie) => (
+        {part.map((movie) => (
             <MoviesCard
               key={movie.id || movie.movieId}
               movie={movie}
@@ -22,10 +60,14 @@ function MoviesCardList({
             />
           ))}
       </ul>
+        {amount >= movies.length ? null : (
         <button
-        className="movies-card-list__more-button"
-        aria-label="Load more movies">
-        Еще </button>
+          className="movies-card-list__more-button"
+          aria-label="Load more movies"
+          onClick={handleMoreCards}>
+          Еще
+        </button>
+      )}
     </section>
   );
 }
