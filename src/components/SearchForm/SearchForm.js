78555211922903a361063a9ba1d830ee114f4536
MariaRez <maria.v.reznik@yandex.ru>
2023-02-14 15:50:26 +0400

    fix: delete validation in search for next work

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
-import useFormWithValidation from "../../utils/useFormWithValidation";
 import "./SearchForm.css";
 
 function SearchForm({ handleSearch, setIsChecked, setPreloader, isLoading }) {
-  const { values, handleChange, errors, isValid, resetForm } =
-    useFormWithValidation();
-  const submitButtonClassName = `search-form__button ${
-    !isValid && "search-form__button_disabled"
-  }`;
+
   const [keyword, setKeyword] = useState(""); // ключевые слова для поиска
   const [isShortMovie, setIsShortMovie] = useState(false); // короткометражный фильм
-
-  useEffect(() => {
-    resetForm("", "", false);
-  }, [resetForm]); //сброс формы
 
   // переключаткль чек бокса
   function onCheckboxToggle(checked) {
@@ -23,7 +14,6 @@ function SearchForm({ handleSearch, setIsChecked, setPreloader, isLoading }) {
   }
   // переключатель для ключевых слов поиска
   function handleKeyword(evt) {
-    handleChange(evt); // из формы валидации берем переключатель
     setKeyword(evt.target.value); // устанавливаем значение для поля с ключевыми словами
   }
   // функция отправки - передвем слова для поиска и прелоудер на отображение
@@ -45,22 +35,21 @@ function SearchForm({ handleSearch, setIsChecked, setPreloader, isLoading }) {
           type="text"
           name="keyword"
           id="keyword"
-          required
+          // required
           placeholder="Введите ключевое слово для поиска"
-          value={values.keyword || ""}
+          value={keyword}
           onChange={handleKeyword}
           disabled={isLoading}
         />
         <button
-          className={submitButtonClassName}
+          className="search-form__button"
           type="submit"
-          disabled={!isValid}
         >
           Поиск
         </button>
       </form>
       <span className="search-form__error" id="keywords-error">
-        {errors.keyword || ""}
+        {/* {errors} */}
       </span>
       <FilterCheckbox onCheckboxToggle={onCheckboxToggle} />
     </div>
